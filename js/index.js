2d4147e10b1c5c2b3058a46aafbcf7f00c3f9994
LipeMachado <silvalipe765@gmail.com>
2021-03-28 19:25:51 -0300

    Em andamento

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ function enviarEmail(event) {
     event.preventDefault();
     fetch('https://emailer-api.herokuapp.com/sender', {
         method: 'POST',
-        body: {
+        headers: {
+            'content-type':'application/json'
+        },
+        body: JSON.stringify({
             emailFrom: document.getElementById('email').nodeValue,
             emailTo: 'silvalipe765@gmail.com',
             name: document.getElementById('name').value,
             message: document.getElementById('message').value
-        }    
+        })
     })
     .then(() => alert('Enviado com Sucesso!'))
     .catch(() => alert('Erro ao enviar o email!'))
