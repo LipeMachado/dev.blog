818a5212754867fe1023046052efc02238c7f827
LipeMachado <silvalipe765@gmail.com>
2021-03-28 18:23:58 -0300

    Finalizando JS

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function enviarEmail(event) {
+    event.preventDefault();
+    fetch('https://emailer-api.herokuapp.com/sender', {
+        method: 'POST',
+        body: {
+            emailFrom: document.getElementById('email').nodeValue,
+            emailTo: 'silvalipe765@gmail.com',
+            name: document.getElementById('name').value,
+            message: document.getElementById('message').value
+        }    
+    })
+    .then(() => alert('Enviado com Sucesso!'))
+    .catch(() => alert('Erro ao enviar o email!'))
+}
